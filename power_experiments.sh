87f67db72601f032ba26fe72d3ba5a60112f2c36
woutkl <wout.klingele@gmail.com>
2020-05-06 11:03:44 +0200

    First power results

--- power_experiments.sh
+++ power_experiments.sh
@@ -6,8 +6,10 @@ TOWN=$1
 FRAMERATE=$2
 IT=$3
 FPSSTEP=60
-BASEDIR=/home/wklingel/carla_release/_benchmark_results/TOWN${TOWN}/1800s_200p0v/${FRAMERATE}/imgs/it${IT}/
+FDIR=/home/wklingel/carla_release/_benchmark_results/TOWN${TOWN}/1800s_200p0v/${FRAMERATE}/
+BASEDIR=${FDIR}imgs/it${IT}/
 DARKNETDIR=/home/wklingel/darknet-mine/
+
 
 if [[ $FRAMERATE == *"fps" ]]; then
     FPSSTEP=${FRAMERATE:0:2}
@@ -16,14 +18,15 @@ if [ ! -d $BASEDIR ]; then
     echo "No such images saved!"
     exit 1
 fi
-
+if [ ! -f ${BASEDIR}/imagepaths.txt ]; then
+    echo File with frame numbers does not exist yet
+    ls ${BASEDIR} | grep png | sed -r 's/^.+\///' | sed "s/^[0-9]*_\([0-9]*\).*$/\1/g" | sed "s/.png$//g" > ${BASEDIR}imagepaths.txt
+fi
 if [ ! -f ${BASEDIR}/movie.mp4 ]; then
     echo Movie does not exist yet
     ffmpeg -r 60 -f image2 -pattern_type glob -i "${BASEDIR}*.png" ${BASEDIR}movie.mp4
-fi
-if [ ! -f ${BASEDIR}/imagepaths.txt ]; then
-    echo File with frame numbers does not exist yet
-    ls -1 ${BASEDIR}*.png | sed -r 's/^.+\///' | sed "s/^[0-9]*_\([0-9]*\).*$/\1/g" | sed "s/.png$//g" > ${BASEDIR}imagepaths.txt
+    echo "Movie created, experiment ended to eliminate movie creation power consumption"
+    exit 1
 fi
 
 echo ${FPSSTEP}
@@ -43,4 +46,12 @@ echo "PROCESS TERMINATED"
 echo "Total Consumed Energy: $SUM J"
 AVG_POWER=$(echo "$SUM/$ITS" | bc -l)
 echo "Average Power Consumed: $AVG_POWER W"
+
+COLUMN_NAMES="ItNr;AvgPower(W);TotalEnergy(J)"
+if [ ! -f ${FDIR}power_results.csv ]; then
+        echo ${COLUMN_NAMES} >> ${FDIR}power_results.csv
+fi
+
+echo "$3;$AVG_POWER;$SUM" >> ${FDIR}power_results.csv
+
 exit 0
